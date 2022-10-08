d097c0bb55d0bd893d881ce71a414f8d46bea5b8
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-10-08 17:44:18 +0300

    5oct group1 exercise

---
+++ 5oct_group1/intro.rkt
@@ -0,0 +1,62 @@
+#lang racket
+
+(define (fact n)
+  (if (= n 0)
+      1
+      (* n (fact (- n 1)))
+  )
+  )
+
+(define (fib n)
+  (if (< n 2)
+      1
+      (+ (fib (- n 1)) (fib (- n 2)))
+      )
+  )
+
+(define (sum-interval a b)
+  (if (> a b)
+      0
+      (+ a (sum-interval (+ a 1) b))
+      )
+  )
+
+(define (count-digits n)
+  (if (< n 10)
+      1
+      (+ 1 (count-digits (quotient n 10)))
+      )
+  )
+
+(define (reverse-digits n)
+  (define (merge a b)
+    (+ (* a (expt 10 (count-digits b))) b)
+    )
+  (if (< n 10)
+      n
+      (merge (remainder n 10) (reverse-digits (quotient n 10)))
+      )
+  )
+
+(define (rev n)
+  (define (first n)
+    (if (< n 10)
+        n
+        (first (quotient n 10))
+        )
+    )
+  (define (rem-first n)
+    (if (< n 10)
+        0
+        (+ (remainder n 10) (* (rem-first (quotient n 10)) 10))
+        )
+    )
+  (if (< n 10)
+      n
+      (+ (first n) (* 10 (rev (rem-first n))))
+      )
+  )
+
+(define (palindrome? n)
+  (= n (rev n))
+  )
