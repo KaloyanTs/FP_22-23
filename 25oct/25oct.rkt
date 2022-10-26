026911c9685aec9efb08feebc2bbd7c2ef96c42e
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-10-26 15:26:39 +0300

    finished past exa problems

--- 25oct/25oct.rkt
+++ 25oct/25oct.rkt
@@ -4,8 +4,8 @@
   (if( > a b) nv
      (op (term a)
          (accumulate op nv (next a) b term next)
+         )
      )
-  )
   )
 
 (define (done? n)
@@ -28,25 +28,55 @@
 (define (sum-almost-done a b)
   (define (min a b) (if (< a b) a b))
   (define (isGood? n)
-    (define (dist-to-done n)
+    (define (dist-to-done n dir)
       (cond ((done? n) 0)
             ((or (< n a) (> n b)) (- (+ b 1) a))
-            (else (min
-                   (dist-to-done (+ n 1))
-                   (dist-to-done (- n 1))
-                   ))
+            (else
+             (cond
+               ((zero? dir)
+                (min
+                 (+ 1 (dist-to-done (+ n 1 ) 1))
+                 (+ 1 (dist-to-done (- n 1) -1))
+                 ))
+               ((> dir 0) (+ 1 (dist-to-done (+ n 1) 1)))
+               (else (+ 1 (dist-to-done (- n 1) -1)))
+               )
+             )
             )
       )
     (define (is-first-least? a b c)
       (and (< a b) (< a c))
       )
-    (is-first-least? (dist-to-done n) (abs (- n a)) (abs (- n b)))
+    (is-first-least? (dist-to-done n 0) (abs (- n a)) (abs (- n b)))
     )
   (accumulate +
               0
               a
               b
               (lambda (i) (if (isGood? i) i 0))
+              (lambda (i) (+ i 1))
+              )
+  )
+
+(define (product-digits n)
+  (if (< n 10) n (* (remainder n 10) (product-digits (quotient n 10)))))
+
+(define (curly n)
+  (- n (product-digits n))
+  )
+
+(define (largest-diff a b)
+  (define (diff-fixed n a b)
+    (if (> a b) 0
+        (max (abs (- (curly n) (curly a)))
+             (diff-fixed n (+ a 1) b))
+        )
+    )
+  (accumulate max
+              0
+              a
+              b
+              (lambda (i) (diff-fixed i (+ i 1) b))
               (lambda (i) (+ i 1))
               )
   )
