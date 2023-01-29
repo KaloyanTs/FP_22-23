1286fa93a4a2478a27995b432c144589c4edb81b
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-01-30 00:42:08 +0200

    finished last problem

--- final_exam_prep/main.hs
+++ final_exam_prep/main.hs
@@ -36,10 +36,43 @@ type Goals = Int
 type Result = (Team, Team, Goals, Goals)
 
 allTeams :: [Result] -> [Team]
-allTeams games = undefined
+allTeams games = uniques $ map getHost games ++ map getGuest games
+    where
+        uniques [] = []
+        uniques (x:xs) = x : filter (/= x) xs
+        getHost (h,_,_,_) = h
+        getGuest (_,g,_,_) = g
                     --  points | goals scored | goals conceded
 teamScore :: [Result] -> Team -> (Int, Int, Int)
-teamScore games t = undefined
+teamScore games t = (points, scored, conceded)
+    where
+        points = sum $ map getPoints games
+        getPoints (h,g,hs,gs)
+            | (h == t && hs>gs) || (g==t && gs>hs) = 3 
+            | (h == t || g==t) && gs==hs = 1 
+            | otherwise = 0
+        scored = sum $ map getScored games
+        getScored (h,g,hs,gs)
+            | h == t = hs 
+            | g == t = gs 
+            | otherwise = 0
+        conceded = sum $ map getConceded games
+        getConceded (h,g,hs,gs)
+            | h == t = gs 
+            | g == t = hs 
+            | otherwise = 0
         -- team name |  points | goals scored | goals conceded
 scoreBoard :: [Result] -> [(Team, Int, Int, Int)]
-scoreBoard games = undefined
+scoreBoard games = qsort less $ map (\team -> push team (teamScore games team)) $ allTeams games
+    where
+        push a (b,c,d) = (a,b,c,d)
+        qsort _ [] = []
+        qsort l (x:xs) = qsort l (filter (l x) xs) ++ [x] ++ qsort l (filter (not.l x) xs)
+        less (t1,p1,sc1,c1) (t2,p2,sc2,c2)
+            | p1 > p2 = True
+            | p1 < p2 = False
+            | (sc1 - c1) > (sc2 - c2) = True
+            | (sc1 - c1) < (sc2 - c2) = False
+            | sc1 > sc2 = True
+            | sc1 < sc2 = False
+            | otherwise = True
