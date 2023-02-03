3ee51f825bacede218f722ec1cde6440bf7576ff
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-02-03 18:29:24 +0200

    more problems solved

--- final_exam_prep/main.hs
+++ final_exam_prep/main.hs
@@ -109,6 +109,7 @@ discountFood l = fst $ head $ filter (\(n, d) -> d == res) l
 data Tree a
   = Empty
   | Tnode {val :: a, left :: Tree a, right :: Tree a}
+  deriving (Show)
 
 twins :: (Eq a) => Tree a -> [a]
 twins Empty = []
@@ -133,27 +134,109 @@ level t n = level (left t) (n - 1) ++ level (right t) (n - 1)
 maxDistance l = maximum [sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2) | (x1, y1) <- l, (x2, y2) <- l]
 
 selectionSort [] = []
-selectionSort l = replicate (length (filter (== m) l)) m ++ selectionSort (filter (/=m) l)
-    where
-        m = minimum l
+selectionSort l = replicate (length (filter (== m) l)) m ++ selectionSort (filter (/= m) l)
+  where
+    m = minimum l
 
-type Graph = [(Int,[Int])]
+type Graph = [(Int, [Int])]
 
 children g v = if null res then [] else res
-    where
-        res = snd $ head $  filter ((== v).fst) g
+  where
+    res = snd $ head $ filter ((== v) . fst) g
 
 isReachable :: Graph -> Int -> Int -> Bool
 isReachable g v1 v2 = run g v1 v2 0
-    where
-        run g u v sum
-            | u == v = True
-            | otherwise = any (\w -> sum + w >=0 && run g w v (sum+w)) (children g u)
+  where
+    run g u v sum
+      | u == v = True
+      | otherwise = any (\w -> sum + w >= 0 && run g w v (sum + w)) (children g u)
 
 matchedBy :: String -> [String]
 matchedBy s = map (\t -> before ++ t ++ after) res
-    where
-        smbl = last $ takeWhile (/= '*') s
-        before = init $ takeWhile (/= '*') s
-        after = tail $ dropWhile (/= '*') s
-        res = "" : map (smbl :) res
+  where
+    smbl = last $ takeWhile (/= '*') s
+    before = init $ takeWhile (/= '*') s
+    after = tail $ dropWhile (/= '*') s
+    res = "" : map (smbl :) res
+
+combine :: (t1 -> a) -> (a -> b -> t2) -> (t1 -> b) -> t1 -> t2
+combine f h g x = h (f x) (g x)
+
+equalFunctions :: (Foldable t1, Eq a) => t1 t2 -> (t2 -> a) -> (t2 -> a) -> Bool
+equalFunctions args f g = all (\arg -> f arg == g arg) args
+
+check :: (Eq b, Enum t2) => t2 -> t2 -> [t2 -> b] -> [b -> b -> b] -> Bool
+check a b uns bins = any (\f -> any (equalFunctions [a .. b] f) uns) [combine f h g | f <- uns, g <- uns, h <- bins]
+
+type Plant = (String, Int, Int)
+
+name :: Plant -> String
+name (n, _, _) = n
+
+minT :: Plant -> Int
+minT (_, m, _) = m
+
+maxT (_, _, m) = m
+
+garden :: [Plant] -> ((Int, Int), [String])
+garden plants = head $ qsort less $ [((mi, ma), map name $ filter (\p -> appr p (mi, ma)) plants) | mi <- [overallMin .. overallMax], ma <- [mi .. overallMax]]
+  where
+    appr p (mi, ma) = minT p <= mi && maxT p >= ma
+    overallMin = minimum $ map minT plants
+    overallMax = maximum $ map maxT plants
+    less x y
+      | length (snd x) > length (snd y) = True
+      | length (snd x) < length (snd y) = False
+      | otherwise = snd (fst x) - fst (fst x) > snd (fst y) - fst (fst y)
+
+qsort :: (a -> a -> Bool) -> [a] -> [a]
+qsort l [] = []
+qsort l (x : xs) =
+  qsort l (filter (`l` x) xs)
+    ++ [x]
+    ++ qsort
+      l
+      (filter (not . (`l` x)) xs)
+
+maxPath :: Graph -> Int -> [Int]
+maxPath g u = iter u [u]
+  where
+    iter :: Int -> [Int] -> [Int]
+    iter v path = longest $ map (\u -> iter u (path ++ [u])) $ filter (`notElem` path) $ children g v
+      where
+        longest [] = path
+        longest l = foldr1 (\x y -> if length x > length y then x else y) l
+
+largestInterval f g a b = foldr1 (\x y -> if snd x - fst x > snd y - fst y then x else y) [(x, y) | x <- [a .. b], y <- [x .. b], all (\x -> f x == g x) [x .. y]]
+
+intervalTree :: (Ord a) => Tree a -> Tree (a, a)
+intervalTree Empty = Empty
+intervalTree t@(Tnode r _ _) = e1 $ iter t r
+  where
+    iter :: (Ord a) => Tree a -> a -> (Tree (a, a), a, a)
+    iter Empty prev = (Empty, prev, prev)
+    iter (Tnode el l r) prev = (Tnode (mi, ma) (e1 resl) (e1 resr), mi, ma)
+      where
+        resl = iter l el
+        resr = iter r el
+        mi = minimum [el, e2 resl, e2 resr]
+        ma = maximum [el, e3 resl, e3 resr]
+    e1 (x, _, _) = x
+    e2 (_, x, _) = x
+    e3 (_, _, x) = x
+
+sumOfSquares = filter (\x -> (not . null) [(a, b) | a <- [1 .. x], b <- [1 .. a], a ^ 2 + b ^ 2 == x]) [1 ..]
+
+type Video = (String, Int)
+
+videoName :: Video -> String
+videoName (n, _) = n
+
+videoLength :: Video -> Int
+videoLength (_, l) = l
+
+-- averageVideo :: [Video] -> String
+averageVideo l = videoName $ foldr1 (\v1 v2 -> if videoLength v1 > videoLength v2 then v1 else v2) $ filter ((<= avg) . videoLength) l
+  where
+    lengths = map videoLength l
+    avg = sum lengths `div` length lengths
